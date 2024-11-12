fa6f97c50cd941d6270dcb3fc8cab70054f2172b
Sjors <456732@student.fontys.nl>
2024-11-12 14:19:46 +0100

    Update alternative reasons in AlternativeSeeder for clarity and relevance + images

--- database/seeders/AlternativeSeeder.php
+++ database/seeders/AlternativeSeeder.php
@@ -11,31 +11,23 @@ class AlternativeSeeder extends Seeder
     public function run(): void
     {
         $alternatives = [
-            ['product_id' => 1, 'alternative_product_id' => 2, 'reason' => 'Biologisch alternatief.'],
-            ['product_id' => 1, 'alternative_product_id' => 3, 'reason' => 'Gezonder alternatief.'],
+            ['product_id' => 1, 'alternative_product_id' => 2, 'reason' => '37% minder uitstoot per kg'],
+            ['product_id' => 1, 'alternative_product_id' => 3, 'reason' => 'Vegan optie'],
             
-            ['product_id' => 4, 'alternative_product_id' => 5, 'reason' => 'Vegetarisch alternatief.'],
-            ['product_id' => 4, 'alternative_product_id' => 6, 'reason' => 'Ecologisch alternatief.'],
+            ['product_id' => 4, 'alternative_product_id' => 5, 'reason' => 'Vaker vanuit Nederland'],
+            ['product_id' => 4, 'alternative_product_id' => 6, 'reason' => 'Altijd vanuit Nederland'],
        
-            ['product_id' => 7, 'alternative_product_id' => 17, 'reason' => 'Biologisch alternatief.'],
-            ['product_id' => 7, 'alternative_product_id' => 18, 'reason' => 'Vegetarisch alternatief.'],
-            ['product_id' => 7, 'alternative_product_id' => 19, 'reason' => 'Gezonder alternatief.'],
-            ['product_id' => 7, 'alternative_product_id' => 20, 'reason' => 'Lokaal geproduceerd.'],
-
-            ['product_id' => 10, 'alternative_product_id' => 21, 'reason' => 'Minder verpakking.'],
-            ['product_id' => 10, 'alternative_product_id' => 22, 'reason' => 'Duurzamer alternatief.'],
-            ['product_id' => 10, 'alternative_product_id' => 23, 'reason' => 'Lagere CO2-uitstoot.'],
-            ['product_id' => 10, 'alternative_product_id' => 24, 'reason' => 'Fair trade gecertificeerd.'],
-
-            ['product_id' => 13, 'alternative_product_id' => 25, 'reason' => 'Biologisch alternatief.'],
-            ['product_id' => 13, 'alternative_product_id' => 26, 'reason' => 'Vegetarisch alternatief.'],
-            ['product_id' => 13, 'alternative_product_id' => 27, 'reason' => 'Gezonder alternatief.'],
-            ['product_id' => 13, 'alternative_product_id' => 28, 'reason' => 'Lokaal geproduceerd.'],
-
-            ['product_id' => 16, 'alternative_product_id' => 29, 'reason' => 'Minder verpakking.'],
-            ['product_id' => 16, 'alternative_product_id' => 30, 'reason' => 'Duurzamer alternatief.'],
-            ['product_id' => 16, 'alternative_product_id' => 31, 'reason' => 'Lagere CO2-uitstoot.'],
-            ['product_id' => 16, 'alternative_product_id' => 32, 'reason' => 'Fair trade gecertificeerd.'],
+            ['product_id' => 7, 'alternative_product_id' => 8, 'reason' => '2 beterleven sterren.'],
+            ['product_id' => 7, 'alternative_product_id' => 9, 'reason' => '3 beterleven sterren'],
+   
+            ['product_id' => 10, 'alternative_product_id' => 11, 'reason' => 'Biologisch en fairtrade'],
+            ['product_id' => 10, 'alternative_product_id' => 12, 'reason' => 'Duurzamer type fruit'],
+ 
+            ['product_id' => 13, 'alternative_product_id' => 14, 'reason' => 'Minder melk verbruik'],
+            ['product_id' => 13, 'alternative_product_id' => 15, 'reason' => 'Biologische keuze'],
+ 
+            ['product_id' => 16, 'alternative_product_id' => 17, 'reason' => 'Niet gekweekt'],
+            ['product_id' => 16, 'alternative_product_id' => 18, 'reason' => 'Duurzamer type vis'],
 
             ['product_id' => 19, 'alternative_product_id' => 81, 'reason' => 'Biologisch alternatief.'],
             ['product_id' => 19, 'alternative_product_id' => 82, 'reason' => 'Vegetarisch alternatief.'],
