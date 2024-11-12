52a18dd82c89f2aa065d7ba2b5c4d2b828c7aa50
Sjors <456732@student.fontys.nl>
2024-11-12 11:48:45 +0100

    Update BrandSeeder to include brand IDs for improved tracking and clarity

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -15,53 +15,53 @@ class BrandSeeder extends Seeder
     {
         $brands = [
             // Meat Brands
-            ['name' => 'Albert Heijn'],
-            ['name' => 'Jumbo'],
-            ['name' => 'Vion'],
-            ['name' => 'Kipster'],
-            ['name' => 'De Vegetarische Slager'],
+            ['name' => 'Albert Heijn'], // ID: 1
+            ['name' => 'Jumbo'], // ID: 2
+            ['name' => 'Vion'], // ID: 3
+            ['name' => 'Kipster'], // ID: 4
+            ['name' => 'De Vegetarische Slager'], // ID: 5
             // Vegetable Brands
-            ['name' => 'Bonduelle'],
-            ['name' => 'Hak'],
-            ['name' => 'Ekoplaza'],
-            ['name' => 'Greenyard'],
-            ['name' => 'Nature\'s Pride'],
+            ['name' => 'Bonduelle'], // ID: 6
+            ['name' => 'Hak'], // ID: 7
+            ['name' => 'Ekoplaza'], // ID: 8
+            ['name' => 'Greenyard'], // ID: 9
+            ['name' => 'Nature\'s Pride'], // ID: 10
             // Fruit Brands
-            ['name' => 'Chiquita'],
-            ['name' => 'Dole'],
-            ['name' => 'Zespri'],
-            ['name' => 'Pink Lady'],
-            ['name' => 'Driscoll\'s'],
+            ['name' => 'Chiquita'], // ID: 11
+            ['name' => 'Dole'], // ID: 12
+            ['name' => 'Zespri'], // ID: 13
+            ['name' => 'Pink Lady'], // ID: 14
+            ['name' => 'Driscoll\'s'], // ID: 15
             // Dairy Brands
-            ['name' => 'Campina'],
-            ['name' => 'Arla'],
-            ['name' => 'Alpro'],
-            ['name' => 'FrieslandCampina'],
-            ['name' => 'Danone'],
+            ['name' => 'Campina'], // ID: 16
+            ['name' => 'Arla'], // ID: 17
+            ['name' => 'Alpro'], // ID: 18
+            ['name' => 'FrieslandCampina'], // ID: 19
+            ['name' => 'Danone'], // ID: 20
             // Bread and Bakery Brands
-            ['name' => 'Bakker Bart'],
-            ['name' => 'Bolletje'],
-            ['name' => 'Van der Meulen'],
-            ['name' => 'Peijnenburg'],
-            ['name' => 'Lotus'],
+            ['name' => 'Bakker Bart'], // ID: 21
+            ['name' => 'Bolletje'], // ID: 22
+            ['name' => 'Van der Meulen'], // ID: 23
+            ['name' => 'Peijnenburg'], // ID: 24
+            ['name' => 'Lotus'], // ID: 25
             // Beverage Brands
-            ['name' => 'Coca-Cola'],
-            ['name' => 'Pepsi'],
-            ['name' => 'Heineken'],
-            ['name' => 'Spa'],
-            ['name' => 'Lipton'],
+            ['name' => 'Coca-Cola'], // ID: 26
+            ['name' => 'Pepsi'], // ID: 27
+            ['name' => 'Heineken'], // ID: 28
+            ['name' => 'Spa'], // ID: 29
+            ['name' => 'Lipton'], // ID: 30
             // Snack Brands
-            ['name' => 'Lay\'s'],
-            ['name' => 'Doritos'],
-            ['name' => 'Calvé'],
-            ['name' => 'Unox'],
-            ['name' => 'M&M\'s'],
+            ['name' => 'Lay\'s'], // ID: 31
+            ['name' => 'Doritos'], // ID: 32
+            ['name' => 'Calvé'], // ID: 33
+            ['name' => 'Unox'], // ID: 34
+            ['name' => 'M&M\'s'], // ID: 35
             // Other Grocery Brands
-            ['name' => 'Knorr'],
-            ['name' => 'Maggi'],
-            ['name' => 'Honig'],
-            ['name' => 'Conimex'],
-            ['name' => 'Blue Band'],
+            ['name' => 'Knorr'], // ID: 36
+            ['name' => 'Maggi'], // ID: 37
+            ['name' => 'Honig'], // ID: 38
+            ['name' => 'Conimex'], // ID: 39
+            ['name' => 'Blue Band'], // ID: 40
         ];
 
         foreach ($brands as $brand) {
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,142 +20,141 @@ class ProductSeeder extends Seeder
                 'name' => 'AH Runder Hamburger 2 stuks',
                 'description' => 'Hamburgers gemaakt van mild gekruid rundvlees met een rulle structuur.',
                 'price' => 3.19,
-                'categorie_id' => 1, // Bladgroenten
+                'categorie_id' => 1,
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 1
             [
                 'barcode' => '8718452050020',
-                'name' => 'Hak Bloemkool',
-                'description' => 'Verse bloemkool van Hak.',
-                'price' => 2.49,
-                'categorie_id' => 7, // Koolsoorten
-                'brand_id' => 7, // Hak
+                'name' => 'AH Runder Kip burger 2 stuks',
+                'description' => 'Hamburgers gemaakt van mild gekruid rundvlees en kip met een rulle structuur.',
+                'price' => 2.99,
+                'categorie_id' => 1, 
+                'brand_id' => 1,
                 'user_id' => 1,
             ], // ID: 2
             [
                 'barcode' => '8718452050181',
-                'name' => 'AH Broccoli',
-                'description' => 'Verse broccoli van Albert Heijn.',
-                'price' => 1.79,
-                'categorie_id' => 7, // Koolsoorten
+                'name' => 'AH Terra plantaardige burger 2 stuks',
+                'description' => 'Deze luxe plantaardige burger kan niet ontbreken bij de zomerse BBQ! Serveer hem op een broodje met knapperige sla, tomaat en een zoetzure augurk.',
+                'price' => 3.19,
+                'categorie_id' => 1, 
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 3
             [
                 'barcode' => '8718452050198',
-                'name' => 'AH Wortelen',
-                'description' => 'Verse wortelen van Albert Heijn.',
-                'price' => 1.29,
-                'categorie_id' => 6, // Bladgroenten
+                'name' => 'Jazz appels schaal',
+                'description' => 'Zo’n knapperige, friszoete, sappige appel - dat kan alleen een JAZZ™ appel zijn!',
+                'price' => 3,59,
+                'categorie_id' => 13, 
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 4
-            // Vlees
             [
                 'barcode' => '8718452050037',
-                'name' => 'AH Rundvlees',
-                'description' => 'Vers rundvlees van Albert Heijn.',
-                'price' => 10.99,
-                'categorie_id' => 1, // Rundvlees
+                'name' => 'AH Biologisch Royal gala schaal',
+                'description' => 'Royal Gala is een volzoete handappel met een toegankelijke smaak met tonen van nectarine, een echte allemansvriend en biologisch bovendien!',
+                'price' => 2,69,
+                'categorie_id' => 13,
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 5
             [
                 'barcode' => '8717163690012',
-                'name' => 'Jumbo Kipfilet',
-                'description' => 'Verse kipfilet van Jumbo.',
-                'price' => 5.49,
-                'categorie_id' => 2, // Kip
-                'brand_id' => 2, // Jumbo
+                'name' => 'AH Elstar schaal',
+                'description' => 'Elstar is de meest gegeten appel van Nederland, Deze friszoete appel met fijn cassisaroma is een echte alleskunner. ',
+                'price' => 2.19,
+                'categorie_id' => 13,
+                'brand_id' => 1,
                 'user_id' => 1,
             ], // ID: 6
             [
                 'barcode' => '8718452050204',
-                'name' => 'AH Gehakt',
-                'description' => 'Vers gehakt van Albert Heijn.',
-                'price' => 4.99,
-                'categorie_id' => 1, // Rundvlees
+                'name' => 'AH Scharrel kipdijfilet',
+                'description' => 'Deze scharrel kipdijfilet zijn van puur scharrel kippendijvlees waarvan het bot is verwijderd. Deze kipmedaillons zijn ook lekker voor op de barbecue.',
+                'price' => 4.78,
+                'categorie_id' => 2,
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 7
             [
                 'barcode' => '8718452050211',
-                'name' => 'AH Biefstuk',
-                'description' => 'Verse biefstuk van Albert Heijn.',
-                'price' => 12.99,
-                'categorie_id' => 1, // Rundvlees
+                'name' => 'AH Franse scharrelkipfilet',
+                'description' => 'De scharrelkipfilet is van Franse afkomst en heerlijk mals van smaak. Een veelzijdig stukje vlees waar je eindeloos mee kunt variëren.',
+                'price' => 7,07,
+                'categorie_id' => 2,
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 8
             // Vis
             [
                 'barcode' => '8718452050044',
-                'name' => 'AH Visfilet',
-                'description' => 'Verse visfilet van Albert Heijn.',
+                'name' => 'AH Biologisch Kipfilet 2 stuks',
+                'description' => 'Deze malse kipfilet is 100% naturel en daarom geschikt om in veel gerechten te verwerken.',
                 'price' => 8.99,
-                'categorie_id' => 3, // Vis
+                'categorie_id' => 2,
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 9
             [
                 'barcode' => '8718452050051',
-                'name' => 'AH Zalmfilet',
-                'description' => 'Verse zalmfilet van Albert Heijn.',
-                'price' => 12.99,
-                'categorie_id' => 3, // Vis
+                'name' => 'AH Bananen tros',
+                'description' => 'De banaan is de bekendste tropische fruitsoort en een echte favoriet.',
+                'price' => 1.55,
+                'categorie_id' => 13,
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 10
             [
                 'barcode' => '8718452050228',
-                'name' => 'AH Kabeljauwfilet',
-                'description' => 'Verse kabeljauwfilet van Albert Heijn.',
-                'price' => 14.99,
-                'categorie_id' => 3, // Vis
+                'name' => 'AH Biologisch Fairtrade bananen',
+                'description' => 'Het favoriete fruit van Nederland. Deze bananen zijn Fairtrade en biologisch .',
+                'price' => 2.19,
+                'categorie_id' => 13,
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 11
             [
                 'barcode' => '8718452050235',
-                'name' => 'AH Tonijnsteak',
-                'description' => 'Verse tonijnsteak van Albert Heijn.',
-                'price' => 16.99,
-                'categorie_id' => 3, // Vis
+                'name' => 'AH Conference schaal',
+                'description' => 'Conference is een sappige en veelzijdig peer. ',
+                'price' => 2.19,
+                'categorie_id' => 13, 
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 12
             // Varken
             [
                 'barcode' => '8718452050068',
-                'name' => 'AH Varkenshaas',
-                'description' => 'Verse varkenshaas van Albert Heijn.',
-                'price' => 9.99,
-                'categorie_id' => 4, // Varkensvlees
+                'name' => 'AH Goudse jong belegen 48+ stuk',
+                'description' => 'De lekkerste alledaagse Goudse kaas met een milde smaak. Gemaakt van 100% weidemelk.',
+                'price' => 6,74,
+                'categorie_id' => 11, 
                 'brand_id' => 1, // Albert Heijn
                 'user_id' => 1,
             ], // ID: 13
             [
                 'barcode' => '8718452050075',
-                'name' => 'AH Speklapjes',
-                'description' => 'Verse speklapjes van Albert Heijn.',
-                'price' => 6.99,
-                'categorie_id' => 4, // Varkensvlees
+                'name' => 'AH Italiaanse mozzarella',
+                'description' => 'Italiaanse mozzarella gemaakt uit 100% koemelk.',
+                'price' => 1,89,
+                'categorie_id' => 11, 
                 'brand_id' => 1,
                 'user_id' => 1,
             ], // ID: 14
             [
                 'barcode' => '8718452050242',
-                'name' => 'AH Varkensfilet',
-                'description' => 'Verse varkensfilet van Albert Heijn.',
-                'price' => 8.99,
-                'categorie_id' => 4, // Varkensvlees
+                'name' => 'AH Biologisch Jong belegen 50+ stuk',
+                'description' => 'AH biologische kaas wordt gemaakt van de lekkerste biologische koemelk, bereid met vegetarisch stremsel en heeft een mooie volle smaak',
+                'price' => 6,72,
+                'categorie_id' => 11,
                 'brand_id' => 1,
                 'user_id' => 1,
             ], // ID: 15
             [
                 'barcode' => '8718452050259',
-                'name' => 'AH Varkensribbetjes',
+                'name' => 'AH Zalmfilet',
                 'description' => 'Verse varkensribbetjes van Albert Heijn.',
                 'price' => 7.99,
                 'categorie_id' => 4, // Varkensvlees
