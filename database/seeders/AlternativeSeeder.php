3eff3f30de5f4831a667ebd6f9a9cf7a7c17b828
nickvanhooff <nickvanhooff03@gmail.com>
2024-11-12 14:55:49 +0100

    Refactor alternative_product_id to alternative_id in AlternativeSeeder for consistency

--- database/seeders/AlternativeSeeder.php
+++ database/seeders/AlternativeSeeder.php
@@ -10,24 +10,24 @@ class AlternativeSeeder extends Seeder
     public function run(): void
     {
         $alternatives = [
-            ['product_id' => 1, 'alternative_product_id' => 2, 'reason' => '37% minder uitstoot per kg'],
-            ['product_id' => 1, 'alternative_product_id' => 3, 'reason' => 'Vegan optie'],
-            
-            ['product_id' => 4, 'alternative_product_id' => 5, 'reason' => 'Vaker vanuit Nederland'],
-            ['product_id' => 4, 'alternative_product_id' => 6, 'reason' => 'Altijd vanuit Nederland'],
-       
-            ['product_id' => 7, 'alternative_product_id' => 8, 'reason' => '2 beterleven sterren.'],
-            ['product_id' => 7, 'alternative_product_id' => 9, 'reason' => '3 beterleven sterren'],
-   
-            ['product_id' => 10, 'alternative_product_id' => 11, 'reason' => 'Biologisch en fairtrade'],
-            ['product_id' => 10, 'alternative_product_id' => 12, 'reason' => 'Duurzamer type fruit'],
- 
-            ['product_id' => 13, 'alternative_product_id' => 14, 'reason' => 'Minder melk verbruik'],
-            ['product_id' => 13, 'alternative_product_id' => 15, 'reason' => 'Biologische keuze'],
- 
-            ['product_id' => 16, 'alternative_product_id' => 17, 'reason' => 'Niet gekweekt'],
-            ['product_id' => 16, 'alternative_product_id' => 18, 'reason' => 'Duurzamer type vis'],
-            
+            ['product_id' => 1, 'alternative_id' => 2, 'reason' => '37% minder uitstoot per kg'],
+            ['product_id' => 1, 'alternative_id' => 3, 'reason' => 'Vegan optie'],
+
+            ['product_id' => 4, 'alternative_id' => 5, 'reason' => 'Vaker vanuit Nederland'],
+            ['product_id' => 4, 'alternative_id' => 6, 'reason' => 'Altijd vanuit Nederland'],
+
+            ['product_id' => 7, 'alternative_id' => 8, 'reason' => '2 beterleven sterren.'],
+            ['product_id' => 7, 'alternative_id' => 9, 'reason' => '3 beterleven sterren'],
+
+            ['product_id' => 10, 'alternative_id' => 11, 'reason' => 'Biologisch en fairtrade'],
+            ['product_id' => 10, 'alternative_id' => 12, 'reason' => 'Duurzamer type fruit'],
+
+            ['product_id' => 13, 'alternative_id' => 14, 'reason' => 'Minder melk verbruik'],
+            ['product_id' => 13, 'alternative_id' => 15, 'reason' => 'Biologische keuze'],
+
+            ['product_id' => 16, 'alternative_id' => 17, 'reason' => 'Niet gekweekt'],
+            ['product_id' => 16, 'alternative_id' => 18, 'reason' => 'Duurzamer type vis'],
+
             ['product_id' => 19, 'alternative_id' => 81, 'reason' => 'Biologisch alternatief.'],
             ['product_id' => 19, 'alternative_id' => 82, 'reason' => 'Vegetarisch alternatief.'],
             ['product_id' => 19, 'alternative_id' => 83, 'reason' => 'Gezonder alternatief.'],
